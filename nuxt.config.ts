390b421a37b8f3c68337635e087597480a3e4d58
manvanoss <manvanoss@gmail.com>
2025-08-08 15:36:23 +0800

    Setup GitHub Pages deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,17 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+
+    app: {
+    baseURL: process.env.NUXT_APP_BASE_URL ?? '/catnation/', // or '/'
+  },
+
+    nitro: {
+    prerender: {
+      crawlLinks: true,
+      routes: ['/'], // add other routes if needed
+    },
+    },
+
 })

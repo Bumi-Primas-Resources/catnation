738e0c10bdd5af89a8527e95504d5dbd6c650abb
manvanoss <manvanoss@gmail.com>
2025-08-09 13:44:15 +0800

    fix: fixing the tailwind config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.ts
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
-
   app: {
-        baseURL: process.env.NODE_ENV === 'production' ? '/catnation/' : '/', // <-- repo name with leading & trailing slash
+    baseURL: process.env.NODE_ENV === 'production' ? '/catnation/' : '/',
   },
-  
-runtimeConfig: { public: {  apiBase: process.env.NUXT_PUBLIC_API_BASE} }
-,
-
-    nitro: {
-    prerender: {
-      crawlLinks: true,
-      routes: ['/'], // add other routes if needed
+  modules: ['@nuxtjs/tailwindcss'],
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_PUBLIC_API_BASE ,
     },
-    },
-
-
-
+  },
+  nitro: {
+    preset: 'github-pages',
+  },
 })
+
